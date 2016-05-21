5a2e6fdce28a5c63bf0d3e8b0bd22b670b5646dc
Andreas Ekeroot <andeke@gmail.com>
2016-05-21 21:01:30 +0200

    This AI is also quite bad at playing the game.
    But it can see into the future.

--- src/Main.hs
+++ src/Main.hs
@@ -2,8 +2,10 @@ module Main where
 
 import Control.Monad
 import Control.Monad.State
+import Data.Function
 import Data.List
 import Data.Maybe
+import qualified Data.Set as S
 import qualified Data.Vector as V
 import System.Random
 
@@ -21,6 +23,21 @@ rotatingAI d b = do
                    else (b'':) <$> rotatingAI (roundSucc d) b''
 
 
+-- | This AI makes the move that leads to the most number of free squares.
+-- It can see into the future.
+mostFreeAI :: (RandomGen g) => Board -> State g [Board]
+mostFreeAI b = do
+  moves <- fmap catMaybes $ mapM (\d -> ((fmap . fmap) (\b' -> (d, V.length $ freeCells b')) (move d b))) $ S.toList $ validMoves b
+  case moves of
+    [] -> return []
+    xs -> do
+      b' <- move (fst $ maximumBy (compare `on` snd) xs) b
+      case b' of
+        Nothing  -> return []
+        Just b'' -> if hasWon b''
+                       then return [b'']
+                       else (b'':) <$> mostFreeAI b''
+
 -- | Evaluate an AI.
 evalAI :: State StdGen [Board] -> IO [Board]
 evalAI b = do
@@ -28,4 +45,6 @@ evalAI b = do
   return $ evalState b g
 
 main :: IO ()
-main = mapM_ print =<< evalAI (rotatingAI UpD =<< createBoard)
+main = mapM_ print =<< evalAI (mostFreeAI =<< createBoard)
+
+-- main = mapM_ print =<< evalAI (rotatingAI UpD =<< createBoard)
